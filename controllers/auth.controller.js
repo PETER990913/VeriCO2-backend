8e5f992bd72996b93e30d23f477352b4c05df788
TopDevelpoer <133781576+PETER990913@users.noreply.github.com>
2023-08-18 07:56:14 -0700

    'one'

---
+++ controllers/auth.controller.js
@@ -0,0 +1,100 @@
+const db = require("../models");
+const config = require("../config/auth.config");
+const User = db.user;
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+
+
+exports.signup = (req, res) => {
+  // Save User to Database
+  User.create({
+    user: req.body.name,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+    birthday: req.body.birthday,
+    password: bcrypt.hashSync(req.body.password, 8),
+    companyName: req.body.companyName
+    // role: 'user'
+  })
+    .then(user => {
+      res.status(200).send({ user: user });
+    })
+    .catch(err => {
+      res.status(500).send({ errors: err.message, message: 'Something went wrong!' });
+    });
+};
+
+exports.signin = (req, res) => {
+
+  User.findOne({
+    email: req.body.email
+  })
+    .then(async user => {
+      if (!user) {
+        return res.status(401).send({ message: "Invalid email!" });
+      }
+
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (!passwordIsValid) {
+        return res.status(402).send({
+          message: "Invalid password"
+        });
+      }
+      
+      const queryToken = {};
+     
+      /** 2023/5/9 3:30 */
+      /** Add more information */
+      queryToken.id = user.id;
+      queryToken.role = user.role;
+
+      var token = jwt.sign(queryToken, config.secret, {
+        expiresIn: 86400 // 24 hours
+      });
+
+      res.status(200).send({
+        user: user,
+        accessToken: token
+      });
+
+    })
+    .catch(err => {
+      res.status(500).send({ errors: err.message, message: 'Something went wrong!' });
+    });
+};
+
+exports.checkEmail = (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    res.status(200).send({
+      email: user.email,
+      status: true
+    })
+  }) 
+  .catch(err=>{
+    res.status(500).send({ errors: err.message, message: 'Something went wrong!' });
+  })
+}
+
+exports.resetPassword = (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+      user.password = bcrypt.hashSync(req.body.password, 8),
+      user.save().then(()=>{
+        res.send({
+          status: true
+        })
+      })
+      .catch(err=>{
+        res.status(500).send({ errors: err.message, message: 'Something went wrong!' });
+      })
+  }).catch(err=>{
+    res.status(500).send({ errors: err.message, message: 'Something went wrong!' });
+  })
+}
